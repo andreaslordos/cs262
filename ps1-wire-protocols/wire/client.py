f0409224672bf6fe0abcb42f3dde2a1469722848
Adam Mohamed <adamshokrymohamed@gmail.com>
2023-03-05 18:16:11 -0500

    Added login prompt; Updated server/client files
    - Server: Completed handle function
    - Server: Completed login/register prompt function
    - Server: Fixed receive function
    - Client: Completed parse_input function
    - Client: Completed login/register function
    - Client: Updated receive function
    - Added colors class

--- ps1-wire-protocols/wire/client.py
+++ ps1-wire-protocols/wire/client.py
@@ -1,38 +1,35 @@
 import socket, threading
-import sys
+from colors import *
 
 IP_ADDR = '0.0.0.0'
 PORT = 7976
 VERSION_NUMBER = '1'
 
-# to change colors of terminal text
-class bcolors:
-    HEADER = '\033[95m'
-    OKBLUE = '\033[94m'
-    OKCYAN = '\033[96m'
-    OKGREEN = '\033[92m'
-    WARNING = '\033[93m'
-    FAIL = '\033[91m'
-    ENDC = '\033[0m'
-    BOLD = '\033[1m'
-    UNDERLINE = '\033[4m'
+'''
+WIRE PROTOCOL
+================
+    1. Version Number (1 byte)
+    2. Command (1 byte)
+    3. Data (variable length)
+'''
 
 commands = {'PROMPT': '1', # Prompt for login/create account
             'LOGIN': '2', # Request for logging in
             'REGISTER': '3', # Request for creating new account
             'LOGIN_PROMPT': '4', # Request for entering login credentials
             'CREATE_PROMPT': '5', # Request for entering new account credentials
-            'DISPLAY': '6', # Display message
+            'DISPLAY': '6', # Display message to client terminal
             'HELP': '7', # Display help
             'LIST_USERS': '8', #Display list of users
-            'CONNECT': '9', # Connect to a user
+            'CONNECT': '9', # Request to connect to a user
             'TEXT': 'a', # Send text to a user
-            'NOTHING': 'b', # 
-            'DELETE': 'c',
-            'EXIT_CHAT': 'd',
-            'SHOW_TEXT': 'e',
-            'START_CHAT': 'f',
-            'REQUEST': 'g'}
+            'NOTHING': 'b', # When client sends empty message
+            'DELETE': 'c', # Request for deleting account
+            'EXIT_CHAT': 'd', # Request for exiting chat
+            'SHOW_TEXT': 'e', # Display text to client terminal -- prolly not needed, could use display instead
+            'START_CHAT': 'f', # Response to client's request to start chat
+            'QUIT': 'g', # Request for quitting application
+}
 
 class ChatClient:
     def __init__(self, ip, port) -> None:
@@ -40,19 +37,78 @@ class ChatClient:
         self.client.connect((ip, port))
 
 
-    def login(self):
-        self.username = input("Choose a username: ")
-        return VERSION_NUMBER + commands['LOGIN'] + self.username
+    '''
+    Parses input from user
+    Converts cli input to appropriate request format to send to server
+    Format of returned string: <VERSION_NUMBER><COMMAND><DATA>
+    '''
+    def parse_input(self, cli_input: str) -> str:
+        '''
+        Parses input from user
+        Returns the appropriate request
+        '''
+        if not cli_input:
+            return VERSION_NUMBER + commands['NOTHING'] + ''
 
+        cli_input = ''.join(cli_input.split()) # Remove all whitespace from cli_input
+        command = 'TEXT'
+        data = ''
+        if cli_input[0] == '/':
+            if cli_input[1].upper() == 'H':
+                command = 'HELP'
+            elif cli_input[1].upper() == 'L':
+                command = 'LIST_USERS'
+            elif cli_input[1].upper() == 'C':
+                command = 'CONNECT'
+                if len(cli_input) > 2: data = cli_input[2:] # If user enters /C <username> send username to server
+            elif cli_input[1].upper() == 'D':
+                command = 'DELETE'
+            elif cli_input[1].upper() == 'Q':
+                command = 'QUIT'
+
+        return VERSION_NUMBER + commands[command] + data
+
+
+    '''
+    Handles login and registration
+    Prompts user to login or register
+    Returns the appropriate request
+    '''
+    def login_register(self) -> str:
+        # print("Welcome to the chat app! Please choose an option:")
+        # print("1. Login (L)")
+        # print("2. Register (R)")
+        choice = ''
+
+        while choice not in ['L', 'R']:
+            choice = input("Enter your choice: ").upper()
+        
+        if choice == 'L':
+            username = input("Welcome back. Enter your username: ")
+            return VERSION_NUMBER + commands['LOGIN'] + username
+        
+        else:
+            username = input("Enter your new username: ")
+            return VERSION_NUMBER + commands['REGISTER'] + username
+        
 
     def receive(self):
         while True:
             try:
                 message = self.client.recv(1024).decode('utf-8')
                 if message[1] == commands['PROMPT']:
-                    self.client.send(self.login().encode('utf-8'))
+                    if message[2:]: print(message[2:]) # print error message, if any
+                    req = self.login_register().encode('utf-8')
+                    self.client.send(req)
+
+                elif message[1] == commands['DISPLAY']:
+                    if message[2:]: print(message[2:])
+                    response = input(f"{strBlue('-->')} ")
+                    req = self.parse_input(response).encode('utf-8')
+                    client.send(req)
                 else:
                     print(message)
+
             except:
                 print("An error occurred!")
                 self.client.close()
--- ps1-wire-protocols/wire/server.py
+++ ps1-wire-protocols/wire/server.py
@@ -1,44 +1,46 @@
 import socket, threading
-import sys
+from colors import *
 
 HOST = '0.0.0.0'
 PORT = 7976
 VERSION_NUMBER = '1'
 
+'''
+WIRE PROTOCOL
+================
+    1. Version Number (1 byte)
+    2. Command (1 byte)
+    3. Data (variable length)
+'''
 
-# to change colors of terminal text
-class bcolors:
-    HEADER = '\033[95m'
-    OKBLUE = '\033[94m'
-    OKCYAN = '\033[96m'
-    OKGREEN = '\033[92m'
-    WARNING = '\033[93m'
-    FAIL = '\033[91m'
-    ENDC = '\033[0m'
-    BOLD = '\033[1m'
-    UNDERLINE = '\033[4m'
+
+'''
+Might want to add logout command
+Would need to restructure the initial prompting for login/register
+so that it's accessible after logging out.
+'''
 
 commands = {'PROMPT': '1', # Prompt for login/create account
             'LOGIN': '2', # Request for logging in
             'REGISTER': '3', # Request for creating new account
             'LOGIN_PROMPT': '4', # Request for entering login credentials
             'CREATE_PROMPT': '5', # Request for entering new account credentials
-            'DISPLAY': '6', # Display message
+            'DISPLAY': '6', # Display message to client terminal
             'HELP': '7', # Display help
             'LIST_USERS': '8', #Display list of users
-            'CONNECT': '9', # Connect to a user
+            'CONNECT': '9', # Request to connect to a user
             'TEXT': 'a', # Send text to a user
-            'NOTHING': 'b', # 
-            'DELETE': 'c',
-            'EXIT_CHAT': 'd',
-            'SHOW_TEXT': 'e',
-            'START_CHAT': 'f',
-            'REQUEST': 'g'}
-
+            'NOTHING': 'b', # When client sends empty message
+            'DELETE': 'c', # Request for deleting account
+            'EXIT_CHAT': 'd', # Request for exiting chat
+            'SHOW_TEXT': 'e', # Display text to client terminal -- prolly not needed, could use display instead
+            'START_CHAT': 'f', # Response to client's request to start chat
+            'QUIT': 'g', # Request for quitting application
+}
 
 # define ChatServer class
 class ChatServer:
-    def __init__(self, host, port) -> None:
+    def __init__(self, host: str, port: str) -> None:
         self.server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.server.bind((host, port))
         self.server.listen()
@@ -56,15 +58,41 @@ class ChatServer:
         print(f"Server started on {self.host_name} ({self.ip_addr})")
 
 
-    def add_client(self, client):
+    '''
+    For debugging purposes
+    Shows server's global variables
+    '''
+    def show_state(self) -> None:
+        print('*'*80)
+        print('clients:', self.clients)
+        print('users:', self.usernames)
+        print('connections:', self.connections)
+        print('queue:', self.queue)
+        print('logged_in:', self.logged_in)
+
+
+    def help(self) -> str:
+        message = strBlue("Welcome to the chat server!") + \
+        f'''
+        Here are the commands you can use:
+        {strBlue('/H')} - Display this help message
+        {strBlue('/L')} - List all users
+        {strBlue('/C')} <username> - Connect to a user
+        {strBlue('/Q')} - Quit application
+        {strBlue('/D')} - Delete account and exit application
+        '''
+        return VERSION_NUMBER + commands['DISPLAY'] + message
+        
+
+    def add_client(self, client: socket) -> None:
         self.clients[client] = ''
 
     
-    def link_user(self, client, username):
+    def link_user(self, client: socket, username: str) -> None:
         self.clients[client] = username
 
 
-    def remove_client(self, client):
+    def remove_client(self, client: socket) -> None:
         # Remove client from connections and clients objects
         if client in self.connections:
             self.connections.pop(client)
@@ -73,20 +101,81 @@ class ChatServer:
         client.close()
 
 
-    def broadcast(self, message):
+    def broadcast(self, message: str) -> None:
         for client in self.clients:
-            client.send(message)
+            msg = VERSION_NUMBER + commands['DISPLAY'] + message
+            client.send(msg.encode('utf-8'))
 
 
-    def handle(self, client):
+    def handle(self, client: socket) -> None:
+       
+        self.show_state() # For debugging purposes
+
         while True:
             try:
                 message = client.recv(1024)
-                self.broadcast(message)
+                if message[1] == commands['HELP']:
+                    client.send(self.help().encode('utf-8'))
+                elif message[1] == commands['LIST_USERS']:
+                    self.list_users()
+                elif message[1] == commands['CONNECT']:
+                    self.connect(message[2:])
+                elif message[1] == commands['DELETE']:
+                    self.delete_account()
+                elif message[1] == commands['TEXT']:
+                    if message[2:]: self.text(message[2:])
+                elif message[1] == commands['EXIT_CHAT']:
+                    self.exit_chat()
+                elif message[1] == commands['NOTHING']:
+                    pass
+                # Still need to add text, exit chat, and nothing commands
+
             except:
                 self.remove_client(client)
 
+    '''
+    Handles initial login/register prompt. 
+    Takes in client and username.
+    Returns error message if any.
+    '''
+    def login_register(self, client: socket, username: str) -> str:
+        error_msg = ''
+        
+        # Check if request is for login
+        if username[1] == commands['LOGIN']:
+            username = username[2:]
+            
+            # Check if username exists
+            if username not in self.usernames:
+                error_msg = 'Username does not exist'
 
+            # Check if user is already logged in
+            elif username in self.logged_in:
+                error_msg = 'User already logged in'
+
+            else:
+                self.link_user(client, username)
+                self.logged_in.add(client)
+                print(f"User {username} logged in")
+                print(f"Current users: {self.usernames}")
+
+        # Check if request is for creating new account
+        elif username[1] == commands['REGISTER']:
+            username = username[2:]
+            
+            # Check if username already exists
+            if username in self.usernames:
+                error_msg = 'Username already exists'
+
+            else:
+                self.usernames.append(username)
+                self.link_user(client, username)
+                self.logged_in.add(client)
+                self.connections.append({username: []})
+                print(f"New user {username} created")
+                print(f"Current users: {self.usernames}")
+        
+        return error_msg
 
     def receive(self):
         while True:
@@ -95,15 +184,23 @@ class ChatServer:
             client.send("Connected to server".encode('utf-8'))
 
             self.add_client(client)
-            error_msg = ''
+            error_msg = '''
+Welcome to the chat app! Please choose an option:
+1. Login (L)
+2. Register (R)
+            '''
+            # Prompt user to login or create account before creating thread
+            while True:
+                client.send((VERSION_NUMBER + commands['PROMPT'] + error_msg).encode('utf-8'))
+                username = client.recv(1024).decode('utf-8')
+                error_msg = self.login_register(client, username)
+                
+                if error_msg == '':
+                    break
 
-            client.send((VERSION_NUMBER + commands['PROMPT'] + error_msg).encode('utf-8')) 
-            username = client.recv(1024).decode('utf-8')
-            self.usernames.append(username)
-            self.link_user(client, username)
-
-            print(f"Username of client is {username}")
-            self.broadcast(f"{username} has joined the chat!".encode('utf-8'))
+            print(f"New account created with username: {username}") # debugging purposes
+            # Display list of available commands
+            client.send(self.help().encode('utf-8'))
 
             thread = threading.Thread(target=self.handle, args=(client,))
             thread.start()
